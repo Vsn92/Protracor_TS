cdcf73097095d06473ceb2c68c022ec8e44acf05
Vsn92 <santhoshnagaraj1992@gmail.com>
2019-08-11 16:49:22 +0530

    Update conf.ts
    Added Suites in Conf

--- conf.ts
+++ conf.ts
@@ -3,6 +3,12 @@ import { Driver } from 'selenium-webdriver/opera';
 export let config:Config={
     seleniumAddress:'http://localhost:4444/wd/hub',
     specs:['./Specs/BankManagerLoginSpec.js'],
+    suites:{
+        smoke:'./Specs/HomePageSpec.js',
+        customer:'./Specs/CustomerLoginSpec.js',
+        manager:'./Specs/BankManagerLoginSpec.js',
+        e2e:['./Specs/HomePageSpec.js','./Specs/CustomerLoginSpec.js','./Specs/BankManagerLoginSpec.js']
+    },
     //allScriptsTimeout:99999,
     onPrepare(){
        browser.driver.manage().window().maximize();
